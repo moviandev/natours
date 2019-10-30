a0ef79a4c28357eadf4bc6c9849d248fbf14fc1d
moviandev <matheus_o_viana@hotmail.com>
2019-10-30 15:07:26 -0300

    chore: added a controller handler and have been made some refactoring into app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -20,20 +22,9 @@ app.use('/api/v1/users', userRouter);
 
 // Handling routes errors
 app.all('*', (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'Not Found';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'Internal Error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;

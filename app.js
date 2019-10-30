a4f340dd1b37128dca72613d83e95efe58cedf42
moviandev <matheus_o_viana@hotmail.com>
2019-10-30 14:50:49 -0300

    chore: added global error handling

--- app.js
+++ app.js
@@ -20,9 +20,19 @@ app.use('/api/v1/users', userRouter);
 
 // Handling routes errors
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Not Found',
-    message: `Can't find ${req.originalUrl} on this server`
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'Not Found';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'Internal Error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 

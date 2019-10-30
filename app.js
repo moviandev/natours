9cc8d188ac3cabbb947af2327bf15ca80ead6634
moviandev <matheus_o_viana@hotmail.com>
2019-10-30 14:17:25 -0300

    chore: Handling routes errors

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+// ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling routes errors
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'Not Found',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
 
 module.exports = app;

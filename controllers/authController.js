a27f8e6a771d492322197dd641eb508321cbe6e1
moviandev <matheus_o_viana@hotmail.com>
2019-11-06 12:20:39 -0300

    chore: added JSON web token package, and added token into signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+
+const envV = process.env;
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -9,8 +12,13 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
+  const token = jwt.sign({ id: newUser._id }, envV.JWT_TOKEN, {
+    expiresIn: envV.JWT_EXP_IN
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }

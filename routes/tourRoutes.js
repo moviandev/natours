81db14b9cc30d6ad94e69141f939eec2afb23783
Matheus Viana <matheus_o_viana@hotmail.com>
2019-11-21 16:14:09 -0300

    chore: added ppprotection methods

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+const auth = require('./../controllers/authController');
 
 const router = express.Router();
 
@@ -12,7 +13,7 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(auth.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router

5eaccfa6302cff1b1c8bd7555348a4bcce96cb6e
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-04 18:40:24 -0300

    chore: added more secure measures to prevent our api from being attacked

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,10 +22,15 @@ const createSendToken = (user, statusCode, res) => {
     // Here setted the httpOnly option to true to not be modified in broswer in any way
     httpOnly: true
   };
+
   // Here setted the secure option to true to just send this cookie in a encrypted request, but just in production.
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
+  // Sending cookies to client
   res.cookie('jwt', token, cookieOptions);
+
+  // Reomving password from the output of creating a new user
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',

343a6f90db68a96de00ded636532b1c174de8a15
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-04 18:53:08 -0300

    chore: added rate limiting, was set just 100 request from one IP

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,11 +9,23 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//1> MIDDLEWARES
+//1> GLOBAL MIDDLEWARES
 // Um middleware é uma função que pode mudar os dados que estão chegando.
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  // 100 requests per hour, if the api should make more request from one IP this max limiter should be greater
+  max: 100,
+  // 60 minutes * 60 seconds * 1000 milliseconds
+  windowMs: 3600000,
+  message: 'Too many request from this IP, please try again in an hour'
+});
+
+// Here we are applying this limiter to the api route, this should effect all of our routes
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 

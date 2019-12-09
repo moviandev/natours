1aac93f05f85f61351dfdb623a6364854c4aaa31
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-09 18:11:04 -0300

    security: added prevention against HTTP parameter polution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -43,6 +44,20 @@ app.use(mongoSanitize());
 
 // DATA SANATIZATION against XSS
 app.use(xss());
+
+// PREVENT AGAINST HTTP PARAMETER POLUTION
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingQuantity',
+      'avarage',
+      'maxGroupSize',
+      'difficult',
+      'price'
+    ]
+  })
+);
 
 app.use(express.static(`${__dirname}/public`));
 

c599cecbab7aafdfe5dbb8cebdf8d2bae684b94f
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-12 16:28:12 -0300

    delete: deleting unused const

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -1,6 +1,5 @@
 const Review = require('../models/reviewsModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   const review = await Review.find();

dbb4054c2a9ad394f6cb837576feadbe13f7d0a2
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-12 18:43:44 -0300

    feats: added nested GET request

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -2,7 +2,11 @@ const Review = require('../models/reviewsModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const review = await Review.find();
+  let filter = {};
+
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const review = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
